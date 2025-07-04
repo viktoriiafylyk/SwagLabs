72db7b0f12ace5b011abcbd1a7fd22caee4ce76f
viktoriiafylyk <vfilik194@gmail.com>
2025-07-04 16:50:33 +0300

    Cleared imports

--- src/test/java/com/swaglabs/tests/ui/TestFooterLinks.java
+++ src/test/java/com/swaglabs/tests/ui/TestFooterLinks.java
@@ -6,13 +6,7 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Issue;
 import io.qameta.allure.Owner;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
 
 import static org.testng.Assert.*;
 
